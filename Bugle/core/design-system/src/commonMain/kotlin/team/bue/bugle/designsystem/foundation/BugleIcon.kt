666621a3cab5336bd4fd6ba9af588b7670f2b894
유섭 <yoonbk2001027@gmail.com>
2026-02-11 13:47:19 +0900

    feat :: 아이콘 리소스 및 BugleIcon 오브젝트 추가

---
+++ Bugle/core/design-system/src/commonMain/kotlin/team/bue/bugle/designsystem/foundation/BugleIcon.kt
@@ -0,0 +1,32 @@
+package team.bue.bugle.designsystem.foundation
+
+import bugle.core.design_system.generated.resources.Res
+import bugle.core.design_system.generated.resources.ic_cancel
+import bugle.core.design_system.generated.resources.ic_check
+import bugle.core.design_system.generated.resources.ic_check_on
+import bugle.core.design_system.generated.resources.ic_email
+import bugle.core.design_system.generated.resources.ic_error
+import bugle.core.design_system.generated.resources.ic_eye
+import bugle.core.design_system.generated.resources.ic_eye_off
+import bugle.core.design_system.generated.resources.ic_heart
+import bugle.core.design_system.generated.resources.ic_heart_filled
+import bugle.core.design_system.generated.resources.ic_kakao
+import bugle.core.design_system.generated.resources.ic_kakao_disabled
+import bugle.core.design_system.generated.resources.ic_link
+import bugle.core.design_system.generated.resources.ic_link_off
+
+object BugleIcon {
+    val Cancel = Res.drawable.ic_cancel
+    val Check = Res.drawable.ic_check
+    val CheckOn = Res.drawable.ic_check_on
+    val Email = Res.drawable.ic_email
+    val Error = Res.drawable.ic_error
+    val Eye = Res.drawable.ic_eye
+    val EyeOff = Res.drawable.ic_eye_off
+    val Heart = Res.drawable.ic_heart
+    val HeartFilled = Res.drawable.ic_heart_filled
+    val Kakao = Res.drawable.ic_kakao
+    val KakaoDisabled = Res.drawable.ic_kakao_disabled
+    val Link = Res.drawable.ic_link
+    val LinkOff = Res.drawable.ic_link_off
+}
